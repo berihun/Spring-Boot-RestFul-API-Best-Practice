208ad078a2dbf51aa2e5a4d3b4d7358416a42616
Berihun Hadis <berihunhad1@yahoo.com>
2022-05-05 20:56:59 +0300

    Delete server/src/main/java/com/example/server directory

--- server/src/main/java/com/example/server/repo/ServerRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.server.repo;
-
-import com.example.server.model.Serverr;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServerRepo extends JpaRepository<Serverr, Long> {
-    Serverr findByIpAddress(String ipAddress);
-}
